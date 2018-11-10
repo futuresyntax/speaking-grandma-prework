b1e081e9f27b0eebcd45206338215b30a81d4741
FutureScript <fernandobaxter1@gmail.com>
2018-11-10 14:20:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-elsif phrase.downcase
+elsif "phrase.downcase"
   return "Huh?! SPEAK UP, SONNY!"
 else phrase.uppercase
   puts "NO, NOT SINCE 1938!"
