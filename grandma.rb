79c7da19dd575948c2e552673fc3b622c42c2ea1
FutureScript <fernandobaxter1@gmail.com>
2018-11-10 14:19:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ else phrase.uppercase
   puts "NO, NOT SINCE 1938!"
 
 end
-    
+
 end
   # Write a speak_to_grandma method.
 
@@ -15,7 +15,7 @@ end
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
