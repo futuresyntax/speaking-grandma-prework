6d30d066450a90a87e31534cd7314f00a404d291
FutureScript <fernandobaxter1@gmail.com>
2018-11-10 15:13:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,6 +7,9 @@ else phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
   end
 
+end 
+
+  
 
 
 
@@ -16,16 +19,4 @@ else phrase == "I LOVE YOU GRANDMA!"
 
 
 
-  # Write a speak_to_grandma method.
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so)
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
